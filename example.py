7194eb5ec4a2e8a8c6bc183231a1929f1602346a
Light <102204002+aztice@users.noreply.github.com>
2024-09-14 11:27:22 +0800

    Update example.py

--- example.py
+++ example.py
@@ -2,5 +2,5 @@ import iksk
 
 iksk_instance = iksk.iksk()
 result = iksk_instance.encrypt('Hello World', mode=[0, 1, 2, 3, 1])
-print('加密后内容 :'+result)
-print('解密后内容'+iksk_instance.decrypt(result_mode0[0], mode=[0, 1, 2, 3, 1], sn=result_mode0[1]))
+print('加密后内容 :'+result[0])
+print('解密后内容:'+iksk_instance.decrypt(result[0], mode=[0, 1, 2, 3, 1], sn=result[1]))
